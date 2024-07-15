ee8a68c5ce39ef9440f455117723d2ce7dcc9162
Matthieu Beurel <matthieu@mbeurel.com>
2024-07-15 16:19:49 +0200

    feat : Update Symfony version to 6.4

--- AustralNotifyBundle.php
+++ AustralNotifyBundle.php
@@ -21,7 +21,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 class AustralNotifyBundle extends Bundle
 {
 
-  public function build(ContainerBuilder $container)
+  public function build(ContainerBuilder $container): void
   {
     parent::build($container);
     $container->addCompilerPass(new MessengerPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 1000);
